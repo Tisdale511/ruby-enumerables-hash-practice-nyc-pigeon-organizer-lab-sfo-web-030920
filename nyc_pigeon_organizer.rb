7bea37c25ca786ae2581564d788b18f2823c5fe7
Tisdale Fry <tisdalefry@gmail.com>
2020-03-05 21:55:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(data)
   name_hash = {}
   data.each do |data, color|
-    
-    
+    color.each do |colors, names|
+      if !name_hash[colors]
+        name_hash[colors] = names
+    end
   end
 end
